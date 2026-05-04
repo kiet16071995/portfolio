d2c9451dcd85c6c31b69d47b58fa49f82e602cb7
buituankiet16071995-dotcom <buituankiet16071995@gmail.com>
2026-05-04 10:20:01 +0700

    first update

--- src/data.ts
+++ src/data.ts
@@ -16,21 +16,21 @@ export const PersonalInfo: AboutMeData = {
 
 export const games: Game[] = [
   {
-    name: "GAME_NAME",
-    description: "GAME_DESCRIPTION",
-    genres: ["GAME_GENRE", "GAME_GENRE..."],
+    name: "ROUGELIKE BTK",
+    description: "A simple rougelike topdown shooter",
+    genres: ["Topdown shooter", "Rougelike"],
     platforms: [Platform.Windows],
     engine: GameEngine.Unity,
     links: [
-      { source: LinkImageSource.Github, url: "https://github.com/YOUR_REPO" },
-      { source: LinkImageSource.ItchIo, url: "https://YOUR_ITCH.itch.io/GAME_NAME" },
-    ],
+      { source: LinkImageSource.Github, url: "https://github.com/buituankiet16071995-dotcom/RougelikeBTK" },
+      { source: LinkImageSource.ItchIo, url: "https://kiet16071995.itch.io/rougelike-btk?secret=b340rEih6tz2pY8fqr3swPO1Vo
     media: [
-      { source: "/images/games/GAME_NAME/Screenshot_1.png", type: MediaType.Image },
+      { source: "/images/games/rougelike-btk/Untitled.jpg", type: MediaType.Image },
       { source: "https://www.youtube.com/embed/dQw4w9WgXcQ", type: MediaType.YouTube },
-      { source: "/images/games/GAME_NAME/Screenshot_2.png", type: MediaType.Image },
-      { source: "/images/games/GAME_NAME/Screenshot_3.png", type: MediaType.Image },
-      { source: "/images/games/GAME_NAME/Screenshot_4.png", type: MediaType.Image },
+      { source: "/images/games/rougelike-btk/Untitled1.jpg", type: MediaType.Image },
+      { source: "/images/games/rougelike-btk/Untitled2.jpg", type: MediaType.Image },
+      { source: "/images/games/rougelike-btk/Untitled4.jpg", type: MediaType.Image },
+      { source: "/images/games/rougelike-btk/Untitled3.jpg", type: MediaType.Image },
     ],
   },
   {
